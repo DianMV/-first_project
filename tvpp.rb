4241064ea189733a5160b3bcb2b16c9b73cf7b4e
DianMV <dinogana123@gmail.com>
2026-03-01 21:57:59 -0500

    comments were added abd another condition

--- tvpp.rb
+++ tvpp.rb
@@ -9,14 +9,21 @@ num=gets.chomp.to_i
 
 
   middle=(notas.length - 1)/2
-  if num<notas[middle]
-  (0..middle).reverse_each do |i|
+  if num<notas[middle] #since the array is in order, we would only take into accounts the elements before the middle one if num was smaller than the middle one
+  (0..middle).reverse_each do |i| #using reverse to avoid skiping the next element when an element repeats twice
     if notas[i]==num
       notas.delete_at(i)
     end
   end
-  elsif num>notas[middle]
-    (middle..notas.length).reverse_each do |i|
+  elsif num>notas[middle] 
+    (middle..notas.length - 1).reverse_each do |i| #In case the middle number was smaller than num, we would take the numbers from the middle to the last element since num could only repeat itself further ahead
+      if notas[i]==num
+        notas.delete_at(i)
+      end
+    end
+  
+  elsif num==notas[middle]
+    (0..notas.length - 1).reverse_each do |i|
       if notas[i]==num
         notas.delete_at(i)
       end
